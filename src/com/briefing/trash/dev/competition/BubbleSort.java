2a3eac7b2b706cbaf6acb01b05ca0cae7c2370e8
Y_Hirayama <y.hirayama@nicosys.jp>
2017-06-18 23:59:35 +0900

    新規作成

---
+++ src/com/briefing/trash/dev/competition/BubbleSort.java
@@ -0,0 +1,52 @@
+package com.briefing.trash.dev.competition;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+/**
+ * TODO イケてない
+ * Bulletクラスに依存している
+ */
+public class BubbleSort implements ISort<Bullet>{
+
+    @Override
+    public Bullet[] sort(Bullet[] bullets) {
+        // ソート開始
+        final LocalDateTime sortStart = LocalDateTime.now();
+        System.out.println("## SORT START " + sortStart);
+
+        // バブルソート
+        Bullet[] ret = BubbleSort.bubbleSort(bullets);
+
+        // ソート終了
+        final LocalDateTime sortFinish = LocalDateTime.now();
+        System.out.println("### END   " + sortFinish);
+        System.out.println("### takes " + sortStart.until(sortFinish, ChronoUnit.MILLIS));
+        
+        return ret;
+    }
+    
+    /**
+     * 
+     * @param bullets
+     * @return
+     */
+    private static Bullet[] bubbleSort(Bullet[] bullets) {
+        Bullet tmp;
+        int bulletsSize = bullets.length;
+
+        for (int i = 0; i < bulletsSize; i++)
+        {
+            for (int j = i + 1; j < bulletsSize; j++)
+            {
+                if (bullets[i].compareTo(bullets[j]) > 0)
+                { // XXX 昇順の場合小さい場合の方が次第に少なくなる気がするからisSmallerThanを利用したけど大丈夫か？
+                    tmp = bullets[i];
+                    bullets[i] = bullets[j];
+                    bullets[j] = tmp;
+                }
+            }
+        }
+        return bullets;
+    }
+}
---
+++ src/com/briefing/trash/dev/competition/ISort.java
@@ -0,0 +1,6 @@
+package com.briefing.trash.dev.competition;
+
+public interface ISort<T> {
+
+    T[] sort(T[] array);
+}
