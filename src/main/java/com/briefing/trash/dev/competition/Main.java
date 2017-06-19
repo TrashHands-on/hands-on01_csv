4787e458325b8ed3b39504ebd724fcb7751eeb18
TrashHands-on <trbriefing@gmail.com>
2017-06-19 22:40:30 +0900

    第1回ハンズオン大会用リボジトリ作成

--- src/main/java/com/briefing/trash/dev/competition/Main.java
+++ src/main/java/com/briefing/trash/dev/competition/Main.java
@@ -2,12 +2,10 @@ package com.briefing.trash.dev.competition;
 
 import java.io.FileNotFoundException;
 
-public class SortPractice {
+public class Main {
 
 
     public static void main(String[] args) throws FileNotFoundException {
 
-        String fileInput = "/hands-on01_csv/resources/csv/input.csv";
-        String fileOutput = "/hands-on01_csv/resources/csv/output.csv";
     }
 }
